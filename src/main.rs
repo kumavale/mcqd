1258b9c7d1892d2d71d7777ac9633b52f39bc25d
kmgy <bhnk1270223@gn.iwasaki.ac.jp>
2020-01-29 10:53:18 +0900

    prototype

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        eprintln!("invalid argument");
+        eprintln!("Usage:\n    cargo run <answer sheet> <answer key>");
+        std::process::exit(1);
+    }
+
+    let answer_sheet = fs::read_to_string(&args[1])?;
+    let answer_key   = fs::read_to_string(&args[2])?;
+
+    let answer_sheet = answer_sheet.split_whitespace();
+    let mut answer_key = answer_key.split_whitespace();
+    let mut answers_sum = 0;
+    let mut correct_answers = 0;
+
+    for (i, sheet) in answer_sheet.enumerate() {
+        let key   = answer_key.next().unwrap();
+        answers_sum += 1;
+
+        if sheet == key {
+            correct_answers += 1;
+            println!("{}. \x1b[32m{}\x1b[0m", i+1, sheet);
+        } else {
+            println!("{}. \x1b[31m{} : {}\x1b[0m", i+1, sheet, key);
+        }
+    }
+
+    let percentage = correct_answers as f32 / answers_sum as f32 * 100_f32;
+    println!("\n{}/{} ... {}%", correct_answers, answers_sum, percentage);
+
+    Ok(())
 }
+
